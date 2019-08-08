b7069ed7275a2ff8b7a9df746d89f28562b6c631
Osborn <jc7483@nyu.edu>
2019-08-08 17:35:16 +0800

    fixed some problems

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch
 from torch.autograd import Variable
 import torch.optim as optim
 import numpy as np
-
+from torch.nn import DataParallel
 from six.moves import cPickle
 
 import opts
@@ -30,6 +30,10 @@ opt = opts.parse_opt()
 #opt.proxy="http://xxxx.xxxx.com:8080"
 
 
+
+
+
+
 if "CUDA_VISIBLE_DEVICES" not in os.environ.keys():
     os.environ["CUDA_VISIBLE_DEVICES"] =opt.gpu
 #opt.model ='lstm'
@@ -39,10 +43,11 @@ if from_torchtext:
     train_iter, test_iter = utils.loadData(opt)
 else:
     import dataHelper as helper
-    train_iter, test_iter = dataHelper.loadData(opt)
+    train_iter, dev_iter, test_iter = dataHelper.loadData(opt)
 
 opt.lstm_layers=2
 
+torch.manual_seed(0)
 model=models.setup(opt)
 if torch.cuda.is_available():
     model.cuda()
@@ -51,7 +56,8 @@ print("# parameters:", sum(param.numel() for param in model.parameters() if para
 optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=opt.learning_rate)
 optimizer.zero_grad()
 loss_fun = F.cross_entropy
-
+# model = DataParallel(model)             # 并行化model
+# loss  = DataParallel(loss_fun)
 #batch = next(iter(train_iter))
 
 #x=batch.text[0]
@@ -59,25 +65,40 @@ loss_fun = F.cross_entropy
 #x=batch.text[0] #64x200
 
 #print(utils.evaluation(model,test_iter))
+max_precision = 0
 for i in range(opt.max_epoch):
-    for epoch,batch in enumerate(train_iter):
+    model.train()
+    for batch, iterator in enumerate(train_iter):
+
         start= time.time()
- 
-        text = batch.text[0] if from_torchtext else batch.text
+        optimizer.zero_grad()
+        text = iterator.text[0] if from_torchtext else iterator.text
         predicted = model(text)
 
-        loss= loss_fun(predicted,batch.label)
+        loss= loss_fun(predicted,iterator.label)
 
         loss.backward()
         utils.clip_gradient(optimizer, opt.grad_clip)
         optimizer.step()
-        if epoch% 100==0:
+        if batch% 100==0:
             if  torch.cuda.is_available():
-                print("%d iteration %d epoch with loss : %.5f in %.4f seconds" % (i,epoch,loss.cpu().item(),time.time()-start))
+                print("%d epoch %d batch with loss : %.5f in %.4f seconds" % (i+1,batch,loss.cuda().item(),time.time()-start))
             else:
-                print("%d iteration %d epoch with loss : %.5f in %.4f seconds" % (i,epoch,loss.data.numpy()[0],time.time()-start))
+                print("%d epoch %d batch with loss : %.5f in %.4f seconds" % (i+1,batch,loss.data.numpy(),time.time()-start))
+                # print("iteration epoch with loss :  in  seconds")
+                # print(i,epoch,loss.data.numpy(),time.time()-start)
  
-    percision=utils.evaluation(model,test_iter,from_torchtext)
-    print("%d iteration with percision %.4f" % (i,percision))
+    precision, F1 =utils.evaluation(model,dev_iter,from_torchtext)
+    print("%d epoch with precision %.4f and F1-score %.5f on development set" % (i+1,precision, F1))
+    if precision > max_precision:
+        max_precision = precision
+        test_precision, test_F1 =utils.evaluation(model,test_iter,from_torchtext)
+        print("%d epoch with precision %.4f and F1-score %.5f on test set" % (i+1,test_precision, test_F1))
+
+    if from_torchtext:
+        train_iter, test_iter = utils.loadData(opt)
+    else:
+        import dataHelper as helper
+        train_iter, _, _ = dataHelper.loadData(opt)
 
 
